2a81a01dadde535953369b5b63fd963c0bb0ba47
github username <github email address>
2021-03-09 16:37:49 -0800

    fetched users index to render users data with a Read method

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-    debugger;
+    fetchUsers()
 })
+
+    const BASE_URL = "http://localhost:3000"
+
+    //read - fetch users index
+
+    function fetchUsers() {
+        fetch(`${BASE_URL}/users`)
+        .then(resp => resp.json())
+        .then(users => {
+            for (const user of users) {
+                let u = new User(user.id, user.name, user.username, user.email)
+                u.renderUser();
+            }
+        })
+    }
+
+    //create - create a new user
+
+
+    //delete - delete a user
--- src/user.js
+++ src/user.js
@@ -1,7 +1,20 @@
 class User {
-    constructor(name, username, email) {
+    constructor(id, name, username, email) {
+        this.id = id;
         this.name = name;
         this.username = username;
         this.email = email
+    }
+
+    renderUser() {
+        let usersDiv = document.getElementById("users-container")
+
+        usersDiv.innerHTML += 
+        `
+        <ul>
+        <h3>Username: ${this.username}</h3>
+        <li>Name: ${this.name} - Email: ${this.email}</li>
+        </ul>
+        `
     }
 }
